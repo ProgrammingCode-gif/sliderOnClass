a05e337161a93f4e25feb7d5c043efbd4116f70a
Shahzod <shahzodtashev49@gmail.com>
2023-04-24 13:13:09 +0500

    final commit

--- main.js
+++ main.js
@@ -28,16 +28,76 @@ class Slider {
             
             if(index != this.active) {
                 slide.style.transform = `translate${this.dir}(${this.width}px)`;
-                
+
+            }
+            if(index === this.slides.length - 1) {
+                slide.style.transform = `translate${this.dir}(${-this.width}px)`;
+
             }
         })
+
+        this.next.addEventListener('click', () => this.move(this.next));
+        this.prev.addEventListener('click', () => this.move(this.prev));
+
+        if(settings.autoplay) {
+            let interval = setInterval(() => {
+                this.move(this.next)
+
+            }, settings.interval)
+            this.slider.addEventListener('mouseenter', () => {
+                clearInterval(interval);
+            })
+            this.slider.addEventListener('mouseleave', () => {
+                interval = setInterval(() => {this.move(this.next) }, settings.interval);
+            })
+        }
+    }
+
+    move(btn) {
+
+        this.disabledBtns();
+
+        const prevOrNext = btn === this.next ? -this.moveSize : this.moveSize;
+        this.slides.forEach((item, i) => {
+            item.style.transition = '0ms'
+            if(i != this.active) {
+                item.style.transform = `translate${this.dir}(${-prevOrNext}px)`
+            }
+        })
+
+        this.slides[this.active].style.transform = `translate${this.dir}(${prevOrNext}px)`;
+        this.slides[this.active].style.transition = this.timeMove + 'ms';
+
+        if(btn == this.next) {
+            this.active < this.slides.length - 1 ? this.active++ : this.active = 0;
+
+        }else {
+            this.active > 0 ? this.active-- : this.active = this.slides.length - 1;
+
+        }
+
+        this.slides[this.active].style.transform = `translate${this.dir}(0px)`;
+        this.slides[this.active].style.transition = this.timeMove + 'ms';
+    }
+
+    disabledBtns() {
+
+        this.next.disabled = true;
+        this.prev.disabled = true;
+
+        setTimeout(() => {
+            this.next.disabled = false;
+            this.prev.disabled = false; 
+
+        }, this.timeMove);
     }
 }
 
 const slider = new Slider({
+
     slider: '.slider',
     direction: 'x',
     time: 1000,
     autoplay: true,
-    interval: 3000
+    interval: 1000
 });
